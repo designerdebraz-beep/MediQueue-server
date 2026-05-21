a5da2f10b30a00ece86b94e6d6bf1822b5b7a4ea
designerdebraz-beep <designerdebraz@gmail.com>
2026-05-21 16:40:14 +0300

    verify token on server

--- index.js
+++ index.js
@@ -24,24 +24,24 @@ const client = new MongoClient(uri, {
 
 // const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
 
-// const verifyToken = async (req, res, next) => {
-//   const authHeader = req?.headers.authorization;
-//   if (!authHeader) {
-//     return res.status(401).json({ message: "Unauthorized" });
-//   }
-//   const token = authHeader.split(" ")[1];
-//   if (!token) {
-//     return res.status(401).json({ message: "Unauthorized" });
-//   }
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-//   try {
-//     const { payload } = await jwtVerify(token, JWKS);
-//     console.log(payload);
-//     next();
-//   } catch (error) {
-//     return res.status(403).json({ message: "Forbidden" });
-//   }
-// };
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
 
 async function run() {
   try {
